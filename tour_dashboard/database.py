932ea134a3c00e22881e4141f3c94d7c18bfcd65
Mehrshad <mehrshad.pakdel@posteo.de>
2022-07-14 22:58:28 +0200

    Implement mysql support. Refactor database.py to use general sqlalchemy engine.

--- tour_dashboard/database.py
+++ tour_dashboard/database.py
@@ -1,3 +1,5 @@
+from typing import Dict
+
 import sqlalchemy
 from pandas import DataFrame
 from sqlalchemy import create_engine
@@ -5,32 +7,50 @@ from sqlalchemy import create_engine
 import pandas as pd
 from tour_dashboard import utils
 
+def create_mysql_engine(credentials: Dict[str,str]) -> sqlalchemy.engine:
+	"""
+	Create mysql engine to connect to database
+	:param credentials: keys: user, password, host, port, database, table
+	:return: sqlalchemy.engine for mysql databases
+	"""
+	user = credentials["user"]
+	password = credentials["password"]
+	host = credentials["host"]
+	port = credentials["port"]
+	database = credentials["database"]
+	mysql_dialect = "mysql+pymysql"
+	database_url = f"{mysql_dialect}://{user}:{password}@{host}:{port}/{database}"
+
+	engine = create_engine(database_url, echo=False)
+	return engine
+
 def create_sqlite_engine(database_path: str) -> sqlalchemy.engine:
 	sqlite_database_path = 'sqlite:///' + database_path
 	engine = create_engine(sqlite_database_path, echo=False)
 
 	return engine
 
-def load_df_from_database(database_path: str, table_name: str) -> DataFrame:
-	engine = create_sqlite_engine(database_path)
+def load_df_from_database(engine: sqlalchemy.engine, table_name: str) -> DataFrame:
 	query = f"SELECT * FROM {table_name}"
 	df_table = pd.read_sql_query(query, engine)
 	return df_table
 
-def write_df_to_database(df: DataFrame, database_path: str, table_name: str) -> None:
+def table_exists(engine: sqlalchemy.engine, table_name: str) -> bool:
+	# The recommended way to check for existence
+	return sqlalchemy.inspect(engine).has_table(table_name)
 
-	engine = create_sqlite_engine(database_path)
+def write_df_to_database(df: DataFrame, engine: sqlalchemy.engine, table_name: str) -> None:
 
-	if utils.file_exists(database_path):
-		df_table = load_df_from_database(database_path, table_name)
-		entry_already_exists = utils.entry_exists_in_table_data(df, df_table)
-	else:
-		# if database does not exist, entry does not exist either and is False
-		entry_already_exists = False
-
-	if entry_already_exists:
-		pass
-	else:
-		# write to table
+	if not table_exists(engine, table_name):
 		df.to_sql(table_name, con=engine, if_exists='append')
+	else:
+		# if table exists, load table and check if entry already exists
+		df_table = load_df_from_database(engine, table_name)
+		entry_exists = utils.entry_exists_in_table_data(df, df_table)
+
+		if entry_exists:
+			df.to_sql(table_name, con=engine, if_exists='fail')
+		else:
+			df.to_sql(table_name, con=engine, if_exists='append')
+
 
--- tour_dashboard/main.py
+++ tour_dashboard/main.py
@@ -3,16 +3,24 @@ import pandas as pd
 
 pd.set_option("display.max_columns", None)
 
+
 directory = "../data"
 gpx_extension = ".gpx"
 database_path = '../data/data_concat.db'
-table_name = 'tour_data'
+
+resources = "../resources"
+credentials_path = f"{resources}/mysql_credentials.json"
+
+table_name = 'test_table_v01'
 user_name = "user1"
 
 write_to_table = True
 print_table = True
 
 if __name__ == "__main__":
+
+	credentials = utils.parse_json(credentials_path)
+	mysql_engine = database.create_mysql_engine(credentials)
 
 	if write_to_table:
 		gpx_files = utils.create_file_paths_with_extension(directory, gpx_extension)
@@ -22,10 +30,10 @@ if __name__ == "__main__":
 
 			print(df_to_write.head(50))
 
-			database.write_df_to_database(df_to_write, database_path, table_name)
+			database.write_df_to_database(df_to_write, mysql_engine, table_name)
 
 	if print_table:
-		df_table = database.load_df_from_database(database_path, table_name)
+		df_table = database.load_df_from_database(mysql_engine, table_name)
 
 		print(df_table.head(50))
 		print(len(df_table.index))
